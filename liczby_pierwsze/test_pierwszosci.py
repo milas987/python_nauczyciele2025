6627b37cf1f484b38f68983913b0261297df4638
Milena Sochaj <milenasochaj02@gmail.com>
2025-03-22 13:07:15 +0100

    test czy pierwsza

--- liczby_pierwsze/test_pierwszosci.py
+++ liczby_pierwsze/test_pierwszosci.py
@@ -1,10 +1,14 @@
 def czy_pierwsza(liczba: int):
-    for i in range(2, liczba):
+    if liczba < 2:
+        return False
+
+    for i in range(2, int( liczba**0.5)):
         if liczba % i == 0:
             return False
     return True
 
-
 if __name__ == '__main__':
-    print(czy_pierwsza(50))
+    liczba =int(input())
+    wynik=czy_pierwsza(liczba)
+    print(wynik)
 
