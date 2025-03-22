2bae9ac694fc50082205c536463364a78b05d043
Milena Sochaj <milenasochaj02@gmail.com>
2025-03-22 12:03:42 +0100

    sortowanie babelkowe zmiana

--- sortowanie.py
+++ sortowanie.py
@@ -1,6 +1,6 @@
 import wybieranie
+if __name__ =='__main__':
+    dane = [1,6,5,8,2]
+    wybieranie.sortuj_przez_wybieranie(dane)
 
-dane = [1,6,5,8,2]
-wybieranie.sortuj_przez_wybieranie(dane)
-
-print(dane)
+    print(dane)
