ed2b2cfa2b36c30347ff39524db5603d7d7cc6a6
Milena Sochaj <milenasochaj02@gmail.com>
2025-03-22 14:48:22 +0100

    nowe poprawka3

--- liczby_pierwsze/sito_eratostenesa.py
+++ liczby_pierwsze/sito_eratostenesa.py
@@ -1,18 +1,21 @@
 def generuj_liczby_pierwsze(n):
-    dane=[True]*(n+1)
-    dane[0]=dane[1]=False
-    for i in range(2, n+1):
-        if dane[i]==True:
-            for j in range(2, int(n**0.5)+1):
-                dane[j]=False
-    wynik =[]
+    dane = [True] * (n+1)
+    dane[0] = dane[1] = False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if dane[i] == True:
+            for j in range(i ** 2, n + 1, i):
+                dane[j] = False
+
+    wynik = []
     for i in range(n+1):
-        if dane[i]==True:
-            wynik +=[i]
+        if dane[i] == True:
+            wynik += [i]
 
     return wynik
-if __name__=='__main__':
-    liczby_pierwsze= generuj_liczby_pierwsze(120)
+
+if __name__ == '__main__':
+    liczby_pierwsze = generuj_liczby_pierwsze(120)
     print(liczby_pierwsze)
 
 
