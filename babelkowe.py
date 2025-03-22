d51e8612f2119f2fd65a6069636bbd79b27a5b0c
Milena Sochaj <milenasochaj02@gmail.com>
2025-03-22 11:36:40 +0100

    sortowanie babelkowe

---
+++ babelkowe.py
@@ -0,0 +1,7 @@
+dane = [1, 6, 5, 8, 2, 7, 9 ]
+n = len(dane)
+for i in range (0, n-1):
+    for j in range (0, n-1-i):
+        if dane[j] > dane [j+1]:
+            dane[j], dane[j+1]= dane[j+1], dane[j]
+print (dane)
