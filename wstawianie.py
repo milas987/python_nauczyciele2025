45a5eb4c4fa2ad4e5d2497bdfa37d52761bbf57b
Milena Sochaj <milenasochaj02@gmail.com>
2025-03-22 10:42:31 +0100

    wstawianie czerwony na aktualny

--- wstawianie.py
+++ wstawianie.py
@@ -2,10 +2,10 @@ dane = [1, 6, 5, 8, 2]
 n = len(dane)
 
 for i in range (1,n):
-    czerwony=dane[i]
+    aktualny=dane[i]
     for j in range (0, i):
-        if czerwony <dane [j]:
+        if aktualny <dane [j]:
             dane.pop(i)
-            dane.insert(j, czerwony)
+            dane.insert(j, aktualny)
             break
 print(dane)
