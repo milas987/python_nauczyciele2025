f48ff171e2c3d4e7e431fa79c62e4504f274aaf6
Milena Sochaj <milenasochaj02@gmail.com>
2025-03-22 13:23:45 +0100

    test czy pierwsza zmiana 2

--- liczby_pierwsze/test_pierwszosci.py
+++ liczby_pierwsze/test_pierwszosci.py
@@ -2,11 +2,11 @@ def czy_pierwsza(liczba: int):
     if liczba < 2:
         return False
 
-    if liczba % 2 == 0:
+    if liczba % 2 == 0 or liczba % 3==0:
         return False
 
-    for i in range(2, int( liczba**0.5)):
-        if liczba % i == 0:
+    for i in range(6, int( liczba**0.5)+1, 6):
+        if liczba % i == 0 or liczba % i+1==0:
             return False
     return True
 
