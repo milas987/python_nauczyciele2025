921c9167e72e157abdb7798ae86cec961d5ec9f4
milas987 <milenasochaj02@gmail.com>
2025-04-05 14:39:26 +0200

    naiwnie 2

--- napisy/wyszukiwanie metodą naiwna.py
+++ napisy/wyszukiwanie metodą naiwna.py
@@ -1,6 +1,17 @@
 def wyszukaj_naiwnie(sprawdzany, szukany):
-    for i in range(len(sprawdzany)- len(szukany)+1):
-        if sprawdzany[i:i +len(szukany)] == szukany:
+    for i in range(len(sprawdzany)- len(szukany) + 1):
+        if sprawdzany[i:i +len (szukany)] == szukany:
+            return i
+        return None
+def wyszukaj_naiwnie_v2(sprawdzany, szukany):
+        for i in range (len(sprawdzany)- len(szukany) + 1):
+            znaleziono = True
+            for j in range(len(szukany)):
+                if not sprawdzany[i+j] == szukany[j]:
+                    znaleziono == False
+                    break
+        if znaleziono:
+
             return i
         return None
 
