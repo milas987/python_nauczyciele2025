1c1d1367918057c689bf5471f90e5678431bed90
Milena Sochaj <milenasochaj02@gmail.com>
2025-04-06 15:31:13 +0200

    odszyfrowanie

--- szyfrowanie/szyfr_przestawieniowy.py
+++ szyfrowanie/szyfr_przestawieniowy.py
@@ -28,7 +28,21 @@ def szyfruj(napis, klucz):
 
 
 def odszyfruj(napis, klucz):
-    pass
+    wiersze=len (napis)// len(klucz)
+    kolejnosc=indeksy_klucza(klucz)
+    wynik=""
+
+    for wiersz in range(wiersze):
+        poczatek= wiersz*len(klucz)
+        koniec=(wiersz+1)*len(klucz)
+        zaszyfrowana_linia=napis[poczatek:koniec]
+        odszyfrowana_linia= ['']*len (klucz)
+        for indeks, znak in enumerate(zaszyfrowana_linia):
+            odszyfrowana_linia[kolejnosc[indeks]] = znak
+        odszyfrowana_linia = ''.join(odszyfrowana_linia)
+        wynik += odszyfrowana_linia
+    return wynik
+
 
 
 if __name__ == '__main__':
@@ -39,4 +53,4 @@ if __name__ == '__main__':
     print(zaszyfrowany_napis)
 
     odszyfrowany_napis = odszyfruj(zaszyfrowany_napis, klucz)
-    # print(odszyfrowany_napis)
+    print(odszyfrowany_napis)
