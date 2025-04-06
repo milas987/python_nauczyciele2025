ba774a5822871bda0113dd3307e183e70ae6217d
Milena Sochaj <milenasochaj02@gmail.com>
2025-04-06 12:13:56 +0200

    nowe szyfr cezara

---
+++ szyfrowanie/szyfr cezara.py
@@ -0,0 +1,12 @@
+def szyfruj (napis, klucz):
+    pass
+def odszyfruj (napis, klucz):
+
+if__name__=='__main__':
+    napis = "ala ma kota"
+    klucz = 3
+    zaszyfrowany_napis = szyfruj(napis, klucz)
+    print(zaszyfrowany_napis)
+
+    odszyfrowany_napis = odszyfruj(zaszyfrowany_napis, klucz)
+    print (odszyfrowany_napis)
