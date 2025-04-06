0d98db04545d0ca9ede07ccf401e8b4219364b0c
Milena Sochaj <milenasochaj02@gmail.com>
2025-04-06 13:41:32 +0200

    odszyfrowanie

--- szyfrowanie/szyfr cezara.py
+++ szyfrowanie/szyfr cezara.py
@@ -1,34 +1,51 @@
+
 # alfabet = ""
-# for i in range (ord('a'), ord('z')+1):
+# for i in range(ord('a'), ord('z') + 1):
 #     alfabet+=(chr(i))
+import random
+
+alfabet = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+alfabet = [chr(i) for i in range(ord('a'), ord('z') + 1)] + list("ąćęłóźż")
+random.shuffle(alfabet)
+print(alfabet)
 
 
-
-alfabet= [chr(i) for i in range(ord ('a'), ord('z')+1)]
-
-def szyfruj (napis, klucz):
-    szyfr= [alfabet[(i+ klucz)% len (alfabet)] for i in range (len(alfabet))]
-    wynik=''
-    print (szyfr)
+def szyfruj(napis, klucz):
+    szyfr = [alfabet[(i + klucz) % len(alfabet)] for i in range(len(alfabet))]
+    print(szyfr)
+    wynik = ""
     for znak in napis:
-        if znak.islower ():
-            indeks = ord(znak)- ord ('a')
-            zaszyfrowany_znak=szyfr[indeks]
-            wynik+= zaszyfrowany_znak
+        if ord('a') <= ord(znak) <= ord('z'):  # znak.islower():
+            indeks = ord(znak) - ord('a')
+        if znak in alfabet:
+            indeks = alfabet.index(znak)
+            zaszyfrowany_znak = szyfr[indeks]
+            wynik += zaszyfrowany_znak
         else:
-            wynik+=znak
+            wynik += znak
     return wynik
 
 
+def odszyfruj(napis, klucz):
+    szyfr = [alfabet[(i + klucz) % len(alfabet)] for i in range(len(alfabet))]
+    wynik = ""
+    for znak in napis:
 
-def odszyfruj (napis, klucz):
-    pass
+        if znak in alfabet:
 
-if __name__== '__main__':
-    napis = "ala ma kota"
+            zaszyfrowany_znak = alfabet[szyfr.index(znak)]
+            wynik += zaszyfrowany_znak
+        else:
+            wynik += znak
+    return wynik
+
+
+if __name__ == '__main__':
+    napis = "Ala ma żółć"
     klucz = 3
+
     zaszyfrowany_napis = szyfruj(napis, klucz)
     print(zaszyfrowany_napis)
 
     odszyfrowany_napis = odszyfruj(zaszyfrowany_napis, klucz)
-    print (odszyfrowany_napis)
+    # print(odszyfrowany_napis)
