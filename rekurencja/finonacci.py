e7ed5bdca637529fa1b9ab4489ca1e748304fc05
Milena Sochaj <milenasochaj02@gmail.com>
2025-04-26 14:06:09 +0200

    odszyfrowanie

--- rekurencja/finonacci.py
+++ rekurencja/finonacci.py
@@ -1,9 +1,31 @@
-def f (n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+def f_rekurencyjny(n):
+    if n < 2:
+        return n
     else:
-        return f (n-1) + f(n-2)
-if __name__=='__main__':
-    print (f(20))
+        return f_rekurencyjny(n - 1) + f_rekurencyjny(n - 2)
+
+
+def f_iteracyjny(n):
+    if n < 2:
+        return n
+    fn_1 = 1
+    fn_2 = 0
+    for _ in range(2, n + 1):
+        fn = fn_2 + fn_1
+        fn_2 = fn_1
+        fn_1 = fn
+    return fn
+
+
+def f_iteracyjny_skrocony(n):
+    if n < 2:
+        return n
+    fn_2, fn_1 = 0, 1
+    for _ in range(2, n + 1):
+        fn_2, fn_1 = fn_1, fn_2 + fn_1
+    return fn_1
+
+
+if __name__ == '__main__':
+    print(f_iteracyjny_skrocony(15))
+
