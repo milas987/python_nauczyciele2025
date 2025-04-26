92cb66f1abe75f70994bf91c41a6bbc58f103c6d
Milena Sochaj <milenasochaj02@gmail.com>
2025-04-26 14:58:19 +0200

    odszyfrowanie

---
+++ rekurencja/euklides.py
@@ -0,0 +1,26 @@
+def nwd(a, b):
+    while a != b:
+        if a > b:
+            a -= b
+        else:
+            b -= a
+    return a
+
+
+def nwd_rekurencyjne(a, b):
+    if a != b:
+        if a > b:
+            return nwd_rekurencyjne(a - b, b)
+        else:
+            return nwd_rekurencyjne(a, b - a)
+    return a
+
+
+def nwd_modulo(a, b):
+    if a != 0:
+        return nwd_modulo(b % a, a)
+    return b
+
+
+if __name__ == '__main__':
+    print(nwd_modulo(348, 192))
