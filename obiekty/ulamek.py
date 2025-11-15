ceaa5048a2be65f495c903fd13fed766945f62c8
milas987 <milenasochaj02@gmail.com>
2025-11-15 14:52:56 +0100

    2

--- obiekty/ulamek.py
+++ obiekty/ulamek.py
@@ -3,6 +3,19 @@ class Ulamek:
         self.licznik = licznik
         self.mianownik = mianownik
 
+    def czy_calkowita(self):
+        return self.licznik % self.mianownik == 0
+
+    def __str__(self):
+        return f"{self.licznik}/{self.mianownik}"
+
+def porownaj(a: Ulamek, b: Ulamek):
+    wartosc_a = a.licznik / a.mianownik
+    wartosc_b = b.licznik / b.mianownik
+    if wartosc_a < wartosc_b: return -1
+    elif wartosc_a == wartosc_b: return 0
+    else: return 1
+
 if __name__ == '__main__':
-    u = Ulamek(3, 4)
-    print(u.licznik, u.mianownik)
+    u = Ulamek(8, 4)
+    print(u.__str__())
