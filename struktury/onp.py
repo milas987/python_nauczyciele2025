30eb9e4a93391dbd4dfd5bd758dfd5f3f2a6299b
milas987 <milenasochaj02@gmail.com>
2025-12-14 13:03:22 +0100

    zmiana

--- struktury/onp.py
+++ struktury/onp.py
@@ -1,32 +1,33 @@
+@@ -1,5 +1,5 @@
 def pierwszenstwo(operator):
     if  operator in ['-']:
+    if  operator in ['-', '(']:
         return 0
     elif operator in ['+']:
         return 1
-    elif operator in ['*', '/']:
-        return 2
-    return None
-
-def infiksowa_do_postfiksowej(wejscie):
-    wyjscie = []
+@@ -12,10 +12,16 @@ def infiksowa_do_postfiksowej(wejscie):
     stos = []
 
     for token in wejscie:
         if token in ['+', '-', '*', "/"]:
+        if token in ['+', '-', '*', '/']:
             while stos and pierwszenstwo(stos[-1]) >= pierwszenstwo(token):
                 wyjscie.append(stos.pop())
             stos.append(token)
+        elif token == '(':
+            stos.append(token)
+        elif token == ')':
+            while stos and stos[-1] != '(':
+                wyjscie.append(stos.pop())
+            stos.pop() # na pewno '('
         else:
             wyjscie.append(token)
 
-    while stos:
-        wyjscie.append(stos.pop())
-
-
-    return wyjscie
+@@ -27,6 +33,6 @@ def infiksowa_do_postfiksowej(wejscie):
 
 
 if __name__ == '__main__':
     wyrazenie = "12 + 1 + a * b"# * ( b * c + d / e )"
+    wyrazenie = "12 + 2 * ( 3 * 4 + 10 / 5 )"
     wyrazenie = wyrazenie.split(" ")
     print(" ".join(infiksowa_do_postfiksowej(wyrazenie)))
