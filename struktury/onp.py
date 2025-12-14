b4a5cb674ccfdaaf72ed48bfe1ce521dadefbaf7
milas987 <milenasochaj02@gmail.com>
2025-12-14 13:13:10 +0100

    gotowe

--- struktury/onp.py
+++ struktury/onp.py
@@ -1,15 +1,17 @@
-
 def pierwszenstwo(operator):
-    if  operator in ['-']:
     if  operator in ['-', '(']:
         return 0
     elif operator in ['+']:
         return 1
-@@ -12,10 +12,16 @@ def infiksowa_do_postfiksowej(wejscie):
+    elif operator in ['*', '/']:
+        return 2
+    return None
+
+def infiksowa_do_postfiksowej(wejscie):
+    wyjscie = []
     stos = []
 
     for token in wejscie:
-        if token in ['+', '-', '*', "/"]:
         if token in ['+', '-', '*', '/']:
             while stos and pierwszenstwo(stos[-1]) >= pierwszenstwo(token):
                 wyjscie.append(stos.pop())
@@ -23,11 +25,40 @@ def pierwszenstwo(operator):
         else:
             wyjscie.append(token)
 
-@@ -27,6 +33,6 @@ def infiksowa_do_postfiksowej(wejscie):
+    while stos:
+        wyjscie.append(stos.pop())
 
+
+    return wyjscie
+
+def oblicz(wejscie):
+    stos = []
+    for token in wejscie:
+        if token in ['+', '-', '*', '/']:
+            operand_prawy = stos.pop()
+            operand_lewy = stos.pop()
+
+            wynik = None
+            match(token):
+                case '+':
+                    wynik = operand_lewy + operand_prawy
+                case '-':
+                    wynik = operand_lewy - operand_prawy
+                case '*':
+                    wynik = operand_lewy * operand_prawy
+                case '/':
+                    wynik = operand_lewy / operand_prawy
+
+            stos.append(wynik)
+        else:
+            stos.append(float(token))
+    return stos.pop()
 
 if __name__ == '__main__':
-    wyrazenie = "12 + 1 + a * b"# * ( b * c + d / e )"
     wyrazenie = "12 + 2 * ( 3 * 4 + 10 / 5 )"
     wyrazenie = wyrazenie.split(" ")
     print(" ".join(infiksowa_do_postfiksowej(wyrazenie)))
+    wyrazenie_postfiksowe = infiksowa_do_postfiksowej(wyrazenie)
+    print(" ".join(wyrazenie_postfiksowe))
+    wynik = oblicz(wyrazenie_postfiksowe)
+    print(wynik)
