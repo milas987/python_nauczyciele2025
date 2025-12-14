eeb6a3bca138443489fba7a4e537bd1953c783c9
milas987 <milenasochaj02@gmail.com>
2025-12-14 13:03:58 +0100

    zmiana2

--- obiekty/ulamek.py
+++ obiekty/ulamek.py
@@ -10,12 +10,13 @@ class Ulamek:
         return f"{self.licznik}/{self.mianownik}"
 
 def porownaj(a: Ulamek, b: Ulamek):
-    wartosc_a = a.licznik / a.mianownik
-    wartosc_b = b.licznik / b.mianownik
+    wartosc_a = float(a)
+    wartosc_b = float(b)
     if wartosc_a < wartosc_b: return -1
     elif wartosc_a == wartosc_b: return 0
     else: return 1
 
 if __name__ == '__main__':
-    u = Ulamek(8, 4)
-    print(u.__str__())
+    u = Ulamek(2,3)
+    print(float(u))
+    print(porownaj(Ulamek(2, 3), Ulamek(3,4)))
